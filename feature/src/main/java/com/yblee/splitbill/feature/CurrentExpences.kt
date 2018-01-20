dd0cb9351f35ecb610bfdee42be95d8e82f7c798
Ayhan <ayqolpan.kdd@gmail.com>
2018-01-19 16:53:48 -0800

    Setup basic UI

---
+++ feature/src/main/java/com/yblee/splitbill/feature/CurrentExpences.kt
@@ -0,0 +1,12 @@
+package com.yblee.splitbill.feature
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrentExpences : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_expences)
+    }
+}
