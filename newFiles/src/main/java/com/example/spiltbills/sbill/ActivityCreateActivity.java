509dbaea1658a94ae48e2d377e2eadae4dc74680
Ayhan <Ayhan@Ayhans-MacBook-Pro.local>
2018-03-08 15:33:34 -0800

    new files

---
+++ newFiles/src/main/java/com/example/spiltbills/sbill/ActivityCreateActivity.java
@@ -0,0 +1,83 @@
+package com.example.spiltbills.sbill;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created by Ayhan on 2018-02-13.
+ */
+
+public class ActivityCreateActivity extends Activity {
+    public ActivityCreateActivity() {
+        super();
+    }
+    private ActivityModel model;
+    RegisteredAccount user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_activity);
+
+
+
+
+        final EditText numOfPeopleEditText = findViewById(R.id.num_people);
+        final EditText nameText = findViewById(R.id.activity_name);
+
+        model = new ActivityModel( user );
+
+        nameText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        Button createBtn = findViewById(R.id.create_button);
+
+        createBtn.setOnClickListener((view) -> {
+            if(TextUtils.isEmpty(nameText.getText().toString())) {
+                Toast.makeText(getApplicationContext(), "Activity name cannot be empty!", Toast.LENGTH_LONG).show();
+
+            } else if(TextUtils.isEmpty(numOfPeopleEditText.getText().toString()) || Integer.parseInt(numOfPeopleEditText.getText().toString()) == 0 ){
+                Toast.makeText(getApplicationContext(), "Number of people has to grater than 0!", Toast.LENGTH_LONG).show();
+
+            } else {
+                Intent intent = new Intent(getBaseContext(), ContactActivity.class);
+                intent.putExtra("nameOfActivity",nameText.getText().toString());
+                intent.putExtra("numOfPeople",Integer.parseInt(numOfPeopleEditText.getText().toString()));
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
---
+++ newFiles/src/main/java/com/example/spiltbills/sbill/ActivityModel.java
@@ -0,0 +1,82 @@
+package com.example.spiltbills.sbill;
+
+import android.content.Context;
+import android.widget.Button;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+/**
+ * Created by Ayhan on 2018-02-02.
+ */
+
+public class ActivityModel {
+    List<Activity> activities;
+    List<String> contacts;
+    RegisteredAccount owner;
+
+    public ActivityModel (RegisteredAccount owner) {
+        this.activities = new ArrayList<>();
+        this.contacts = new ArrayList<>();
+        this.owner = owner;
+    }
+
+    //Creates an Activity
+    public Activity createActivity(String name, int numOfPpl) {
+        if(isNameValid(name)) {
+            Activity activity = new Activity(name, numOfPpl, owner);
+            activities.add(activity);
+            return activity;
+        }
+
+        System.out.println("activity not created");
+        return null;
+    }
+
+    private boolean isNameValid(String name){
+        Scanner scanner = new Scanner("." + owner.getID());//need a way to access file
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            for(String str : line.split(","))
+                if(str == name)
+                    return false;
+        }
+        return true;
+    }
+
+
+
+    //Search for an Activity
+    public List<Activity> searchActivity(String input) {
+        List<Activity> matchActivities = new ArrayList<>();
+        for ( Activity a : activities){
+            if(a.getName().contains(input)){
+                matchActivities.add(a);
+            }
+        }
+        return matchActivities;
+
+    }
+
+
+    public boolean addAccount(String name) {
+        return owner.addContact(new ActivityAccount(name) );
+    }
+
+//    public boolean removeAccount(String name) {
+//        return owner.deletContact(name);
+//    }
+
+
+
+}
